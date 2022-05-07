b0481c598a76079fa40ff203c6cf61fa0539a419
w4560000 <39783609+w4560000@users.noreply.github.com>
2022-05-07 23:49:19 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reCAPTCHA_Enterprise_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
